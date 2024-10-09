adb591984071976e882aca208bc3fefba1d3df16
sesdevAchary <sesdev.achary@gmail.com>
2024-10-09 06:29:10 +0000

    unfinished project

--- expressLevelOne/server.js
+++ expressLevelOne/server.js
@@ -1,28 +1,60 @@
-import express from 'express'
-import bodyParser from 'body-parser'   // for taking the incoming post reqest body
+// import express from 'express'
+// import bodyParser from 'body-parser'   // for taking the incoming post reqest body
 
-import userRoutes from './routes/user.js'
+// import userRoutes from './routes/user.js'
 
-const PORT = 3001;
-const app = express (); // creating an app using express
+// const PORT = 3001;
+// const app = express (); // creating an app using express
    
 
 
-app.get('/', (req,res)=>{
-    res.send(" hello from Homepage");
-    console.log('[get route]')
+// app.get('/', (req,res)=>{
+//     res.send(" hello from Homepage");
+//     console.log('[get route]')
+// })
+
+// app.use(bodyParser.json());
+
+// app.get("/api",(req,res) =>
+//     { 
+//         res.json(
+//         {
+//             "message":"hello from the api"
+//     })
+//     })
+// app.use('/user',userRoutes)  // specify the path and router handler
+
+
+// //npm server.js
+
+
+// app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`)); // listening for requests from our client side,
+
+
+
+
+
+
+import express from  'express'
+import bodyparser from 'body-parser'   //  // for taking the incoming post reqest body
+
+import userRoutes from './routes/user.js'
+
+const PORT = 3002;
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send(`hello from the route path`)
 })
 
-app.use(bodyParser.json());
+app.use(bodyparser.json());
 
-app.get("/api",(req,res) =>
-    { 
-        res.json(
-        {
-            "message":"hello from the api"
+app.get('/api',(req,res)=>{
+    res.json({
+        "message":"hello from the api path"
     })
-    })
-app.use('/user',userRoutes)  // specify the path and router handler
+})
 
+app.use('/user',userRoutes)   // // specify the path and router handler
 
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`)); // listening for requests from our client side,
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
